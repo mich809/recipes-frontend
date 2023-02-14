cc84d4590750ef744d0a17336888643569097c32
Michael Caridad <caridadmichael@gmail.com>
2023-02-14 14:54:12 -0400

    add font family and box shadow

--- recipe/tailwind.config.cjs
+++ recipe/tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {      
+      boxShadow: {
+      'recipe': '10px 10px 5px 0px rgba(0,0,0,1)',
+    },fontFamily:{
+      'roboto' : 'Roboto'
+    }},
   },
   plugins: [],
 }
